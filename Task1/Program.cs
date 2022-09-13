bd41ae180e64a7737a6117607cd4de007a2c9703
Grig <chiefy@mail.ru>
2022-09-13 20:44:26 +0300

    Add task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 31: Задайте массив из 12 элементов,
+// заполненный случайными числами из 
+// промежутка [-9, 9]. Найдите сумму 
+// отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+// сумма положительных чисел равна 29, сумма 
+// отрицательных равна -20.
+
+
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1); // +1 тк правая скобка не включает число
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+int[] array = GetArray(12, -9, 9);
+
+int positiveSum = 0;
+int negativeSum = 0;
+
+foreach (int el in array) // перебирает элементы массива(значения а не индексы) и смотрит значения
+{
+    if (el > 0)
+    positiveSum += el;
+    else
+    negativeSum += el;
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных чисел = {positiveSum}, Сумма отрицательных чисел = {negativeSum}");
+
+// //int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] res = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         res[i] = new Random().Next(minValue, maxValue + 1);
+//         Console.Write($"{res[i]} ");
+//     }
+//     return res;
+// }
+
+// int[] array = GetArray(12, -9, 9);
+// int positiveSum = 0;
+// int negativeSum = 0;
+
+// foreach(int el in array)
+// {
+//     if(el>0)
+//     positiveSum +=el;
+//     else
+//     negativeSum +=el;
+// }
+// Console.WriteLine();
+// Console.WriteLine($"Сумма положительных чисел = {positiveSum}, Сумма отрицательных чисел = {negativeSum}");
